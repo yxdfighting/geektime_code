f9de5fa0557ebed16c5390ef3aa56bfc944bc40e
yxdfighting <yanxd0818@126.com>
2021-06-21 19:27:50 +0800

    提交作业 提交

--- src/week1/homework/560-和为K的子数组.go
+++ src/week1/homework/560-和为K的子数组.go
@@ -1,5 +1,28 @@
 package homework
 
-func subarraySum(nums []int, k int) int {
+/*
+	区间连续性问题，考虑前缀和 前缀和的下标表示前i个元素之和
+	a[i] + a[i+1] + ....a[n] = s[n] - s[i-1]   i从0到n-1
+*/
 
+func subarraySum(nums []int, k int) int {
+	//构造前缀和数组
+	preList := make([]int, len(nums)+1)
+
+	for i := 1; i < len(preList); i++ {
+		preList[i] = preList[i-1] + nums[i-1]
+	}
+	//fmt.Printf("%v",preList)
+
+	count := 0
+	for i := 1; i < len(preList); i++ {
+		//j表示区间的idx-1
+		for j := 0; j < i; j++ {
+			if preList[i]-preList[j] == k {
+				count++
+			}
+		}
+	}
+
+	return count
 }
